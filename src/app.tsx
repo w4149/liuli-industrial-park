c5ebecc80653a807ff7e8315a76a639bd1c84ab7
w4149 <1696006921@qq.com>
2026-07-14 14:31:23 +0800

    fix: 在app.tsx中设置window.__ENV__，通过DefinePlugin注入环境变量

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,13 @@
 import { Component } from 'react'
 import './app.scss'
 
+;(window as any).__ENV__ = {
+  NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL || '',
+  NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY || '',
+  AMAP_WEB_KEY: process.env.AMAP_WEB_KEY || '320106c641e5603dcde8b521a58ee0c0',
+  AMAP_SECRET_KEY: process.env.AMAP_SECRET_KEY || '',
+}
+
 class App extends Component {
   componentDidMount() {}
 
